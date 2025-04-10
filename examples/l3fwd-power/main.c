5e06d8678b267de7b0e4b365e33e0219764101de
JacopoCarlon <jacopo.equi@gmail.com>
2025-04-10 15:15:59 +0200

    s0a4

--- examples/l3fwd-power/main.c
+++ examples/l3fwd-power/main.c
@@ -681,7 +681,7 @@ get_ipv4_dst_port(struct rte_ipv4_hdr *ipv4_hdr, uint16_t portid,
 		lookup_struct_t *ipv4_l3fwd_lookup_struct)
 {
 	uint32_t next_hop;
-	printf("> calling rte_lpm_lookup ");
+	//printf("> calling rte_lpm_lookup \n");
 	return ((rte_lpm_lookup(ipv4_l3fwd_lookup_struct,
 			rte_be_to_cpu_32(ipv4_hdr->dst_addr), &next_hop) == 0)?
 			next_hop : portid);
