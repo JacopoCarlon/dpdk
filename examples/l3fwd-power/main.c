4b73c2b4f627fc499c484c34c029eb44e9e25a21
JacopoCarlon <jacopo.equi@gmail.com>
2026-04-19 12:25:17 +0200

    important notes !!!!!!

--- examples/l3fwd-power/main.c
+++ examples/l3fwd-power/main.c
@@ -1119,6 +1119,58 @@ static void update_traffic_state(uint64_t current_tsc, bool packet_received, str
 
 
 
+
+
+/*
+Few important notes!
+the two vital pieces of code (clb_umwait and clb_pause) are in lib/power/rte_power_pmd_mgmt.c 
+
+
+- pmd-mgmt=monitor 
+		-> selects the RTE_POWER_MGMT_TYPE_MONITOR						
+		-> in main, it does rte_power_ethdev_pmgmt_queue_enable(...) 	
+			-> which assigns assigns clb=get_monitor_callback			in lib/power/rte_power_pmd_mgmt.c
+		-> get_monitor_callback 										in lib/power/rte_power_pmd_mgmt.c
+		-> does clb_umwait() 											in lib/power/rte_power_pmd_mgmt.c
+		-> which calls rte_power_monitor() 								in lib/eal/x86/rte_power_intrinsics.c
+		-> which executes power_monitor_ops.mwait(tsc_timestamp);		in lib/eal/x86/rte_power_intrinsics.c
+		-> which actually executes intel_umwait.
+
+		as was setup by RTE_INIT(rte_power_intrinsics_init), 
+			which is always run before main . 
+		-> setups intel_umonitor() 										in lib/eal&x86/rte_power_intrinsics.c
+		-> setups intel_umwait() 										in lib/eal&x86/rte_power_intrinsics.c
+
+
+- pmd-mgmt=pause
+		-> selects the RTE_POWER_MGMT_TYPE_PAUSE	
+		-> in main, it does rte_power_ethdev_pmgmt_queue_enable(...)	
+			-> which assigns clb=clb_pause								in lib/power/rte_power_pmd_mgmt.c
+		-> clb_pause													in lib/power/rte_power_pmd_mgmt.c
+		-> which calls either rte_power_pause() or rte_pause()			in lib/power/rte_power_pmd_mgmt.c
+
+		rte_power_pause() 	is _tpause() 								in lib/eal/x86/rte_power_intrinsics.c
+
+		rte_pause() 		is _mm_pause()								in lib/eal/x86/include/rte_pause.h
+			_mm_pause uses #include <emmintrin.h>
+
+	-> in our tests (see clb_pause printf in branching option), 
+			we were ALWAYS running rte_pause, 
+			meaning that treebeard, cplex3, whiskey and lace do not activate the 
+			global_data.intrinsics_support.power_pause, and thus don't support rte_power_pause()
+			.
+
+
+- rte_delay_us(int)								
+		-> is actually void (*rte_delay_us)(unsigned int) = NULL;		in lib/eal/common/eal_common_timer.c
+		-> is rte_delay_us_block(unsigned int us)						in lib/eal/common/eal_common_timer.c
+		-> it always calls rte_pause()									in lib/eal/x86/include/rte_pause.h
+				_mm_pause uses #include <emmintrin.h>
+
+*/
+
+
+
 // J :  jmain_intr_loop HYBRID jhybrid
 static int main_hybrid_loop(__rte_unused void *dummy)
 {
@@ -1549,7 +1601,7 @@ start_rx:
 
 /*
 remember !!! 
-	from : rte_power_pmd_mgmt.h 
+	from : lib/power/rte_power_pmd_mgmt.h 
 		
 	// 
 	// PMD Power Management Type
@@ -1621,7 +1673,7 @@ main_telemetry_loop(__rte_unused void *dummy)
 	
 
 	while (!is_done()) {
-		
+
 		// printf("___ drain tx queue\n"); // this happens !
 		cur_tsc = rte_rdtsc();
 		/*
